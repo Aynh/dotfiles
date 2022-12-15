0dde819ebb53fb4acdcfff350f0b7e74e849323a
Aynh <99479536+Aynh@users.noreply.github.com>
2022-12-15 14:28:02 +0800

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,10 +1,8 @@
 if status is-interactive
 end
 
-zoxide init fish | source
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-# THEME PURE #
-set fish_function_path /home/yasser/.config/fish/functions/theme-pure/functions/ $fish_function_path
-source /home/yasser/.config/fish/functions/theme-pure/conf.d/pure.fish
+zoxide init fish | source
+source (/usr/bin/starship init fish --print-full-init | psub)
