8ea8d07d09951c850980b43e922c3f009047f8b1
Aynh <99479536+Aynh@users.noreply.github.com>
2023-01-04 01:31:48 +0800

    Update .config/fish/functions/mgrun.fish Update .config/fish/functions/noproxy.fish Update .config/fish/functions/setproxy.fish

--- private_dot_config/private_fish/functions/noproxy.fish
+++ private_dot_config/private_fish/functions/noproxy.fish
@@ -1,3 +1,5 @@
 function noproxy
-ALL_PROXY= HTTP_PROXY= HTTPS_PROXY= $argv
+  ALL_PROXY= HTTP_PROXY= HTTPS_PROXY= $argv
 end
+
+complete --exclusive noproxy -a '(__fish_complete_subcommand)' -n 'not __fish_seen_argument -s e'
