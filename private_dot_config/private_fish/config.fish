73239ad181ac13f779a20dfa1f776807296d4465
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-17 18:08:58 +0800

    Update .gtkrc-2.0 Update .config/i3/config Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,3 +6,7 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 
 zoxide init fish | source
 source (/usr/bin/starship init fish --print-full-init | psub)
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
